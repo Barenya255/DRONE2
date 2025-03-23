47704f004ae4d464785a940ae3c8dadbac5870f4
Barenya255 <bknandy98@gmail.com>
2025-03-23 12:21:06 +0530

    fix3

--- DRONE/src/cuda/graph.h
+++ DRONE/src/cuda/graph.h
@@ -1,13 +1,13 @@
 #ifndef DRONE_CUDA_GRAPH
 #define DRONE_CUDA_GRAPH
 
-#include "nccl.h"
+//#include "nccl.h"
 
 #ifdef __cplusplus
 #define cplus __cplusplus
 #undef __cplusplus
 #endif
-//#include "nccl.h"
+#include "nccl.h"
 #ifdef cplus
 #define __cplusplus cplus
 #undef cplus
